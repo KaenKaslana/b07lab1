f6c3d8fdc55e0319875965032617037a63bc6637
KaenKaslana <feixingyue1026@gmail.com>
2024-09-18 14:38:40 -0400

    Updated

---
+++ Polynomial.java
@@ -0,0 +1,9 @@
+public class Polynomial{
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        int z = 4;
+        int result = 3*x*x - 8*y + 4*z - 7;
+        System.out.println("The result is: " + result);
+    }
+}
