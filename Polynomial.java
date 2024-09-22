b7f225131b187efb63ff96a2d84ec47a1c206f93
KaenKaslana <feixingyue1026@gmail.com>
2024-09-22 16:47:19 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,9 +1,40 @@
 public class Polynomial{
-    public static void main(String[] args) {
-        int x = 2;
-        int y = 3;
-        int z = 4;
-        int result = 3*x*x - 8*y + 4*z - 7;
-        System.out.println("The result is: " + result);
+    double[] coefficients;
+    public Polynomial(){
+        coefficients = new double[]{0};
+    }
+    public Polynomial(double[] coef){
+        coefficients = coef;
+    }
+    public Polynomial add(Polynomial p){
+        int length;
+        if (coefficients.length > p.coefficients.length){
+            length = coefficients.length;
+        }
+        else {
+            length = p.coefficients.length;
+        }
+        double[] arr = new double[length];
+        for (int i = 0; i < coefficients.length; i++){
+            arr[i] += coefficients[i];
+        }
+        for (int i = 0; i < p.coefficients.length; i++){
+            arr[i] += p.coefficients[i];
+        }
+        Polynomial result = new Polynomial(arr);
+        return result;
+    }
+    public double evaluate(double x){
+        double result = 0;
+        for (int i = 0; i < coefficients.length; i++){
+            result += coefficients[i] * Math.pow(x, i);
+        }
+        return result;
+    }
+    public boolean hasRoot(double x){
+        if (evaluate(x) == 0){
+            return true;
+        }
+        return false;
     }
 }
